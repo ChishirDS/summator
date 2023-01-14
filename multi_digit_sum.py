85c26a0e28c18881e28d6a69a821249eee405f01
Chishir <somemail@mail.ru>
2023-01-14 12:39:37 +0300

    added function responding a substracting

--- multi_digit_sum.py
+++ multi_digit_sum.py
@@ -1,4 +1,5 @@
 from single_digit_sum import SingleDigitSum
+from mainLF import LF
 
 
 class MultiDigitSum(SingleDigitSum):
@@ -30,23 +31,95 @@ class MultiDigitSum(SingleDigitSum):
             raise ValueError('The value must be written in binary notation in string representation. Only 0 and 1 are allowed')
         
     
-    def parsing(self):
-        
-        amount_register = []
+    def parser(self):
         list_a = list(map(lambda i: int(i), [i for i in self.a]))
         list_b = list(map(lambda i: int(i), [i for i in self.b]))
         list_a.reverse()
         list_b.reverse()
-        p = 0
         
-        for digit in zip(list_a, list_b):
-            
+        return list_a, list_b
+    
+    
+    def get_sum(self):
+        
+        amount_register = []
+        # вызываем метод преобразовывающзий входный значения для работы
+        values = self.parser()
+        
+        p = 0
+        # перебираем числ в двух входных списках одновременно
+        for digit in zip(values[0], values[1]):
+            # передаем по одному в сумматор, с учетом распаковки и регистра переноса
             obj = SingleDigitSum(*digit, p)
             s, p = obj.single_digit_sum()[0], obj.single_digit_sum()[1]
+            # добавляем выходные значения в регистр сдвига суммы
             amount_register.append(s)
-            
+        # забираем регистр переноса как старший разряд
         amount_register.append(p)
+        # разворачиваем список
         amount_register.reverse()
         
         return amount_register
 
+
+    def get_subtracting(self):     
+        # вызываем метод преобразовывающзий входный значения для работы
+        values = self.parser()
+        
+        #разворачиваем вычитаемое для корректной работы
+        values[1].reverse()
+        
+        reverse_code = []
+        # проходим в цикле по всем числам вычитаемого обрабатывая их логическим НЕ
+        # получаем обратный код
+        for i in values[1]:
+      
+            obj = LF(i)
+            
+            reverse_item = obj.logick_not()
+            
+            reverse_code.append(reverse_item)
+            
+        # генерируем список из нулей по длинне обратного кода -1  
+        additional_code = [0 for i in range(len(reverse_code)-1)]
+        #дабавляем к нему единицу и получаем единицу в двоичной системе с учетом количества бит уменьшаемого
+        additional_code.append(1)
+        
+        # приводим все элементы списков к строковым для дальнейшей передачи в метод суммирования
+        additional_code = list(map(lambda i: str(i), additional_code))
+        reverse_code = list(map(lambda i: str(i), reverse_code))
+        new_value0 = list(map(lambda i: str(i), values[0]))
+
+    
+        new_value0.reverse()
+        
+        # склеиваем уменьшаемое и вычитаемое в строки для передачи в метод суммирования
+        one = ''.join(additional_code)
+        two = ''.join(reverse_code)
+        
+        # передаем их в метод суммирования
+        obj2 = MultiDigitSum(one, two)
+        new_value1 = obj2.get_sum()
+        # срезаем игнорируемый бит
+        new_value1 = new_value1[1:len(new_value1)]
+        
+        new_value2 = list(map(lambda i: str(i), new_value1))
+        # склеиваем двочное число с доп. кодом в строку для передачи в метод суммирования
+        res_newV2 = ''.join(new_value2)
+        
+        #передаем
+        obj3 = MultiDigitSum(new_value0, res_newV2)
+        
+        result = obj3.get_sum()
+        
+        # возвращаем результат отсекая игнорируемый бит
+        return result[1:len(result)]
+        
+        
+        
+            
+        
+
+
+f = MultiDigitSum('101010', '101010')
+print(f.get_subtracting())
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from multi_digit_sum import MultiDigitSum
 
 
-f = MultiDigitSum('1011', '1011')
-print(f.parsing())
+f = MultiDigitSum('1111', '1111')
+print(f.get_sum())
